83d8f00a7c8f5aaf4d2c3f2968f0eab88213deac
Luis Paulo Jr <luispaulojr@gmail.com>
2018-11-26 01:38:09 -0200

    webservice

--- README.md
+++
@@ -1,2 +0,0 @@
-# cardapio
-App hibrido (ionic) para a turma de TECINFO 11850 Niterói
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+  echo json_encode(array(
+    array(
+      "titulo" => "Patês Especiais com um toque defumado",
+      "subtitulo" => "Entrada",
+      "imagem" => "https://www.cozinhandopara2ou1.com.br/wp-content/uploads/2016/10/Pat%C3%AAsDefumados_CozinhandoPara2ou1.jpg",
+      "descricao" => "Pão quentinho, recém saído do forno, com um dos Patês Especiais da casa para abrir seu apetite."
+    ),
+    array(
+      "titulo" => "Peito de peru assado com maple syrup",
+      "subtitulo" => "Prato Principal",
+      "imagem" => "https://www.cozinhandopara2ou1.com/wp-content/uploads/2017/11/PeruComMapleSyrup_CozinhandoPara2ou1.jpg",
+      "descricao" => "Peito de peru assado, suculento e delicioso"
+    ),
+    array(
+      "titulo" => "Figos grelhados com creme de queijo e redução de vinho",
+      "subtitulo" => "Sobremesa",
+      "imagem" => "https://www.cozinhandopara2ou1.com.br/wp-content/uploads/2016/06/FigosGrelhadosComCremeDeQueijoEReducaoDeVinho_CozinhandoPara2ou1.jpg",
+      "descricao" => "Sobremesa combina sabores, texturas e temperaturas que a deixam especial."
+    )
+  ));
